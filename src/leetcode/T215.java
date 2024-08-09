8a786ad9491b260636fcd4aa09f9f9a2dcf78179
cqjnqaq <2537411885@qq.com>
2024-08-09 09:19:54 +0800

    优先队列priorityQueue求数组中第k大的数

---
+++ src/leetcode/T215.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @description:求数组中第k大的数
+ * @author: lzy
+ * @time: 2024/8/9 9:08
+ */
+public class T215 {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((t1, t2) -> t2 - t1);
+        for (int num : nums) {
+            pq.offer(num);
+        }
+        for (int i = 1; i < k; i++) {
+            pq.poll();
+        }
+        return pq.peek();
+    }
+}
