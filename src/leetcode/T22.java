9953b6f94b56f3a180a1be73de593119ad40806a
cqjnqaq <2537411885@qq.com>
2024-08-09 09:18:35 +0800

    dfs求有效的括号组合

---
+++ src/leetcode/T22.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @description:括号生成
+ * @author: lzy
+ * @time: 2024/8/9 9:02
+ */
+public class T22 {
+    public List<String> res = new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+        dfs(0,0,n,"");
+        return res;
+    }
+
+    public void dfs(int left, int right, int n, String temp) {
+        if (left == n && right == n) {
+            res.add(temp);
+            return;
+        }
+        if (left < n) {
+            dfs(left+1,right,n,temp+'(');
+        }
+        if (right < n && right < left) {
+            dfs(left,right+1,n,temp+")");
+        }
+    }
+}
