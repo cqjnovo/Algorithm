424f535141976bc68fd0e4be33bbb168b780a904
cqjnqaq <2537411885@qq.com>
2024-08-10 10:25:58 +0800

    中序遍历 原地修改二叉搜索树为双向链表

---
+++ src/niuke/BM30.java
@@ -0,0 +1,33 @@
+package niuke;
+
+/**
+ * @description:二叉搜索树与双向链表
+ * @author: lzy
+ * @time: 2024/8/10 10:02
+ */
+public class BM30 {
+
+    TreeNode head = null;
+
+    TreeNode pre = null;
+
+    public TreeNode Convert(TreeNode pRootOfTree) {
+        inOrder(pRootOfTree);
+        return head;
+    }
+
+    public void inOrder(TreeNode cur) {
+        if (cur == null) {
+            return;
+        }
+        inOrder(cur.left);
+        if (pre != null) {
+            cur.left = pre;
+            pre.right = cur;
+        } else {
+            head = cur;
+        }
+        pre = cur;
+        inOrder(cur.right);
+    }
+}
