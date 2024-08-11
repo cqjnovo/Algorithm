a8bc8646923dead2fa309499c703d2e555959beb
cqjnqaq <2537411885@qq.com>
2024-08-11 16:22:50 +0800

    最长连续递增子序列:dp[i]表示以nums[i]为结尾元素的子序列中最长的递增子序列的长度

---
+++ src/leetcode/T674.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @description:
+ * @author: lzy
+ * @time: 2024/8/11 16:17
+ */
+public class T674 {
+    public int findLengthOfLCIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        int res = 1;
+        Arrays.fill(dp,1);
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i-1]) {
+                dp[i] = dp[i-1] + 1;
+                if (dp[i] > res) {
+                    res = dp[i];
+                }
+            }
+        }
+        return res;
+    }
+}
