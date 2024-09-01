b056f13212d75933021c66781181c1e17c838fa8
cqjnqaq <2537411885@qq.com>
2024-09-01 20:10:48 +0800

    dp:由上和左两个方向递推

---
+++ src/leetcode/dp/T64.java
@@ -0,0 +1,28 @@
+package leetcode.dp;
+
+/**
+ * @description:
+ * @author: lzy
+ * @time: 2024/9/1 19:49
+ */
+public class T64 {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        //初始化dp第0行和第0列
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = grid[i][0] + dp[i-1][0];
+        }
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = grid[0][j] + dp[0][j-1];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i-1][j],dp[i][j-1]) + grid[i][j];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
