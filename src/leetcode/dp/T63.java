a9419081212e4081d2412bc4c1946170291fd0a2
cqjnqaq <2537411885@qq.com>
2024-09-07 18:50:14 +0800

    dp:当第0行或第0列存在障碍物时，初始化这一行/列后面的值为0,其他位置有障碍直接跳过,无障碍的值取决于它上方和左方的值的和

---
+++ src/leetcode/dp/T63.java
@@ -0,0 +1,37 @@
+package leetcode.dp;
+
+/**
+ * @description: 不同路径Ⅱ
+ * @author: lzy
+ * @time: 2024/9/7 18:37
+ */
+public class T63 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        //初始化第0行
+        for (int j = 0; j < n; j++) {
+            if (obstacleGrid[0][j] == 1) {
+                break;
+            }
+            dp[0][j] = 1;
+        }
+        //初始化第0列
+        for (int i = 0; i < m; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                break;
+            }
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    continue;
+                }
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
